109d364857030858bbb559cb0550801d77be691b
b16karhe <37793806+b16karhe@users.noreply.github.com>
2019-03-04 15:05:42 +0100

    fixed serverfile and played around with angular

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-var connectAng = require('connect'),
+var connect = require('connect'),
 serveStatic = require('serve-static');
 
-var ang = connectAng();
+var angular = connect();
+angular.use(serveStatic("node_modules/angular"));
+angular.listen(5000);
 
-ang.use(serveStatic("node_modules/angular"));
-ang.listen(5000);
-
-var connectVue = require('connect'),
-serveStatic = require('serve-static');
-
-var vue = connectVue();
-
+var vue = connect();
 vue.use(serveStatic("node_modules/vue"));
 vue.listen(5001);
